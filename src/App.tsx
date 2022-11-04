3b9456b3a9a7f118e981d00ece1aff29b0279fec
Yogesh Kumar <yogeshsingh2672000@gmail.com>
2022-11-04 11:02:14 +0530

    airdrop sol to devnet

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+// import logo from './logo.svg';
 import './App.css';
+import { Connection, LAMPORTS_PER_SOL, PublicKey } from '@solana/web3.js'
 
 function App() {
+  const [walletAddress, setWalletAddress] = useState("");
+
+  const handleChange = (e: any) => {
+    setWalletAddress(e.target.value);
+  }
+  const handleClick = () => {
+    airdrop(walletAddress);
+  }
+
+  const airdrop = async (address: string) => {
+    const publicKey = new PublicKey(address);
+    const network = "https://api.devnet.solana.com";
+    const conn = new Connection(network, "confirmed");
+    const tx = await conn.requestAirdrop(publicKey, 2 * LAMPORTS_PER_SOL);
+    await conn.confirmTransaction(tx).then(() => {
+      console.log("done")
+      console.log("transaction: ", tx);
+    })
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" onChange={handleChange} value={walletAddress} />
+      <button onClick={handleClick}>Aridrop Sol</button>
     </div>
   );
 }
