20c17996c4fa3608b4b0532d32e332a623bdea5c
Yogesh Kumar <yogeshsingh2672000@gmail.com>
2022-11-11 14:25:56 +0530

    bug fix

--- src/RpcApi/Utilities.ts
+++ src/RpcApi/Utilities.ts
@@ -5,10 +5,11 @@ const conn = new Connection(network, "confirmed");
 const airdrop = async (address: string) => {
     const publicKey = new PublicKey(address);
     const tx = await conn.requestAirdrop(publicKey, 2 * LAMPORTS_PER_SOL);
-    await conn.confirmTransaction(tx).then(() => {
-      console.log("done")
-      console.log("transaction: ", tx);
-    })
+    await conn.confirmTransaction(tx)
+    // .then(() => {
+    //   console.log("done")
+    //   console.log("transaction: ", tx);
+    // })
   }
 const getBalance = async (address: any) => {
     const publicKey = new PublicKey(address);
