65524d1bd7ea097597b63a9f7bdaeb684c85071a
Yogesh Kumar <yogeshsingh2672000@gmail.com>
2022-11-15 17:50:44 +0530

    cleaned the root file

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
 import './App.css';
-// import Footer from './Components/Footer/Footer';
-// import NavBar from './Components/NavBar/NavBar';
-import Background from "./assets/background.mp4"
 import Utilities from './Components/Utilities/Utilities';
 
 function App() {
 
   return (
     <div className="App">
-      {/* <NavBar /> */}
       <Utilities />
-      {/* <Footer /> */}
     </div>
   );
 }
